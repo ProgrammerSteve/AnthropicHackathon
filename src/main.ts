f506f966fb501c385a60a6c9517345ad5e960358
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-05-29 03:57:25 -0500

    updated sidebar for selecting the language and difficulty, enabled production mode

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode, isDevMode } from '@angular/core';
 import { AppModule } from './app/app.module';
 
+if (isDevMode()) {
+  enableProdMode()
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
