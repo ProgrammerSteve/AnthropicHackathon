ceda7aa644cdcd4a5d48fa7e3f4b654f3796683d
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-05-28 20:52:49 -0500

    added observables to variables so components can render, added test api

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,13 +1,30 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.scss']
 })
-export class DisplayComponent {
+export class DisplayComponent implements OnDestroy {
+  private attemptSubscription: Subscription;
+  private correctSubscription: Subscription;
+  attempts = 0
+  correct = 0
+  constructor(public dataService: DataService) {
+    this.attemptSubscription = this.dataService.attempts$.subscribe((value) => {
+      this.attempts = value;
+    });
+    this.correctSubscription = this.dataService.correct$.subscribe((value) => {
+      this.correct = value;
+    });
+  }
   @Input() question = ""
-  @Input() correct = 0
-  @Input() attempts = 0
+
+  ngOnDestroy(): void {
+    this.attemptSubscription.unsubscribe();
+    this.correctSubscription.unsubscribe();
+  }
+
 
 }
